53f5715ea148f56cf7db39c6afff9a1ac26d6d68
Patrick Worth <worthp@worthconsulting.com>
2022-11-22 09:21:25 -0500

    additions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hybrid/GenerateHardwareConfig.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.hybrid;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+@TeleOp (name="h-GenerateHardwareMap", group="Utilities")
+public class GenerateHardwareConfig extends OpMode {
+    @Override
+    public void init() {
+        HardwareDevice device = null;
+        StringBuffer line = new StringBuffer();
+        String name = null;
+        String className = null;
+
+        for (HardwareDevice hardwareDevice : hardwareMap.unsafeIterable()) {
+            line.setLength(0);
+            device = hardwareDevice;
+
+            name = hardwareMap.getNamesOf(device).iterator().next();
+            className = device.getClass().getName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+
+            // ignore Control Hub stuff
+            if (!name.startsWith("Control Hub")) {
+                line.append('[').append(name)
+                        .append("][")
+                        .append(className)
+                        .append("][")
+                        .append(device.getConnectionInfo()).append(']');
+
+                telemetry.addData("", line.toString());
+            }
+        }
+
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Hardware map is generated", "You can stop now");
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        // finish writing config file
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hybrid/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hybrid/MecanumDrive.java
@@ -1,13 +1,11 @@
-package org.firstinspires.ftc.teamcode;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+package org.firstinspires.ftc.teamcode.hybrid;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(name="Hybrid Mecanumn", group = "hybrid")
 public class MecanumDrive extends LinearOpMode {
     private DcMotor backLeft;
     private DcMotor frontLeft;
