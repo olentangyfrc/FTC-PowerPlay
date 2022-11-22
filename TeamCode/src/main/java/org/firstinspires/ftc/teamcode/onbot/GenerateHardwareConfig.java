9a9c5fde0ddd33f3d34b8380ca4ebae42ed08ae1
Patrick Worth <worthp@worthconsulting.com>
2022-11-22 13:26:53 -0500

    renamed hybrid to onbot cuz that is what it is. added new imu sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/GenerateHardwareConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/GenerateHardwareConfig.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.hybrid;
+package org.firstinspires.ftc.teamcode.onbot;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onbot/MecanumDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.hybrid;
+package org.firstinspires.ftc.teamcode.onbot;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
